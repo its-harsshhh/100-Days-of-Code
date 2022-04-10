ff3ffe2cde8671d8e19eab69abcb364341721eee
Harsh <harshpal653@gmail.com>
2022-04-10 11:08:48 +0530

    .

---
+++ April long challenge/AXNODR/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    long long t;
+    cin>>t;
+    
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+ 
+        if(n%4==3||n%4==2)
+        cout<<"3"<<endl;
+        
+        else if(n%4==0)
+        cout<<n+3<<endl;
+        
+        else if(n%4==1)
+        cout<<n<<endl;
+    }
+    return 0;
+ }
