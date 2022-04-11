237dabfb3d383b756463023510beded3c3b64b44
Harsh <harshpal653@gmail.com>
2022-04-11 18:55:03 +0530

    .

---
+++ April long challenge/XORABC/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   // your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        long long a=n & ~(n-1);
+        if(n&1 || n==a) 
+        { cout<<-1<<endl; }
+        else
+        { cout<< a/2 <<" "<<n/2<<" "<<(n-a)/2<<endl; }
+    }
+    return 0;
+}
