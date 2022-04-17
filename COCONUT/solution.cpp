de23b59cc0e813593c02682f0fff3937dd269274
Harsh <harshpal653@gmail.com>
2022-04-17 20:23:34 +0530

    .

---
+++ COCONUT/solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,a,b,i,j;
+	    cin>>x>>y>>a>>b;
+	    i=a/x;
+	    j=b/y;
+	    cout<<i+j<<endl;
+	    
+	}
+	return 0;
+}
