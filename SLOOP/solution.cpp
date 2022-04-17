257cd817007fd0a293eb72fb472c3497972de036
Harsh <harshpal653@gmail.com>
2022-04-17 20:25:24 +0530

    .

---
+++ SLOOP/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int m,s;
+	    cin>>m>>s;
+	    
+	    cout<<(m/s)<<"\n";
+	}
+	return 0;
+}
