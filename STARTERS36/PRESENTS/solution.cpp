a49b2c3de4ca94ffcfc0ca9ebeb0c86617e39af5
Harsh <harshpal653@gmail.com>
2022-04-28 18:53:02 +0530

    .

---
+++ STARTERS36/PRESENTS/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long n,c=0;
+	    cin>>n;
+	    n=n - n/5;
+	    cout<<n<<"\n";
+	    
+	}
+	return 0;
+}
