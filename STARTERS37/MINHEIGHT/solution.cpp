8ef8b9f16fbfcdc425a5a39cb1d2b797fc52877c
Harsh <harshpal653@gmail.com>
2022-05-05 18:27:32 +0530

    .

---
+++ STARTERS37/MINHEIGHT/solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,h;
+	    cin>>x>>h;
+	    if(x>=h)
+	    {
+	        cout<<"yes\n";
+	    }
+	    else
+	    {
+	        cout<<"no\n";
+	    }
+	}
+	return 0;
+}
