bbc51b8181eb3b6fe79cba40c7bb082ec24e945a
Harsh <harshpal653@gmail.com>
2022-05-15 12:20:41 +0530

    .

---
+++ S01E01/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+	    long long n;
+	    cin>>n;
+	        
+	    
+	        if(21<=n){
+	            cout<<"yes\n";
+	        }
+	        else
+	        {
+	            cout<<"no\n";
+	        }
+	    
+	}
+	return 0;
+}
