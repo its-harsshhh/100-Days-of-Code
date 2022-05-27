a28dead4a334321e846f38b02923546bbbb3b121
Harsh <harshpal653@gmail.com>
2022-05-27 20:05:58 +0530

    .

---
+++ CodeChef/Starters40/DOUBTSUPPORT/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int d;
+	cin>>d;
+	if(d<=1600)
+	{
+	    cout<<"yes\n";
+	}
+	else
+	{
+	    cout<<"NO\n";
+	}
+	return 0;
+}
