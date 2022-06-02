0fb9e5672d53a2878dba937e2e960ca99ddea191
Harsh <harshpal653@gmail.com>
2022-06-02 17:09:17 +0530

    day-1 of web dev

---
+++ 30_days_of_web_development/Day-1 (Fizz-Buzz)/javascript.js
@@ -0,0 +1,8 @@
+function toggleText(){
+    var x = document.getElementById("sec");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
