621e94f66932435dce49b0785121f574bf10e000
Harsh <harshpal653@gmail.com>
2022-06-05 21:36:58 +0530

    .

---
+++ CodeChef/June cook off/SONGS/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x,c=0;
+	    cin>>n>>x;
+	    x=x*3;
+	    cout<<n/x<<endl;
+	}
+	return 0;
+}
