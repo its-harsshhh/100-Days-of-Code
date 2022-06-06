f2ac66f66cb482d9f55dc6fb4147d13525a346eb
Harsh <harshpal653@gmail.com>
2022-06-06 17:36:06 +0530

    Day-4

---
+++ 30_days_of_web_development/Day-4 (calculator)/javascript4.js
@@ -0,0 +1,30 @@
+function calc() {
+    
+var textType = Node.textContent ? 'textContent' : 'innerText',
+    
+    num1 = parseFloat(document.getElementById('num1').value) || 0,
+    num2 = parseFloat(document.getElementById('num2').value) || 0,
+    
+    result = document.getElementById('result');
+
+switch (document.getElementById('op').value.replace(/\s/g,'')){
+    
+    case '+':
+        result[textType] = num1 + num2;
+        break;
+    
+    case '-':
+        result[textType] = num1 - num2;
+        break;
+    case '*':
+        result[textType] = num1 * num2;
+        break;
+    case '/':
+        result[textType] = num1 / num2;
+        break;
+
+    default:
+        result[textType] = 'Seriously? You wanted to try math with that operand? Now stop being silly.'
+        break;
+}
+}
