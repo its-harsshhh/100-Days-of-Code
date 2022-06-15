128570f12317ab741c10831986b0abdf3d696af9
Harsh <harshpal653@gmail.com>
2022-06-15 20:55:12 +0530

    .

---
+++ CodeChef/Starters 43/WAITTIME/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int k,x,td;
+	    cin>>k>>x;
+	    
+	    td=7*k;
+	    cout<<td-x<<endl;
+	}
+	return 0;
+}
