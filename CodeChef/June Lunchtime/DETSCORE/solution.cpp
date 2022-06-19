215ebe9eac5d551e4f92771b1aa404c03b0bd567
Harsh <harshpal653@gmail.com>
2022-06-19 22:45:03 +0530

    .

---
+++ CodeChef/June Lunchtime/DETSCORE/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    int g=n/10;
+	    cout<<g*x<<endl;
+	}
+	return 0;
+}
