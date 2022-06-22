abc8c2610eef16e91de54f402239d2b04476be94
Harsh <harshpal653@gmail.com>
2022-06-22 20:27:58 +0530

    .

---
+++ CodeChef/Starters44/AIRHOCKEY/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>=b){
+	        cout<<7-a<<endl;
+	    }
+	    else{
+	        cout<<7-b<<endl;
+	    }
+	}
+	return 0;
+}
