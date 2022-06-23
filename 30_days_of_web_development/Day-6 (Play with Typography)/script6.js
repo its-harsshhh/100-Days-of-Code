698d6335cb634db89f0b41c0681638897d6a5338
Harsh <harshpal653@gmail.com>
2022-06-23 22:58:51 +0530

    Day-6

---
+++ 30_days_of_web_development/Day-6 (Play with Typography)/script6.js
@@ -0,0 +1,26 @@
+WebFont.load({
+		google: {
+			families: ['Vast Shadow:400', 'Comfortaa:400']
+		}
+	});
+
+function function1(){
+	var x = document.getElementById("scroltxt");
+	x.style.fontFamily="monospace";
+}
+function function2(){
+	var x = document.getElementById("scroltxt");
+	x.style.fontFamily="Georgia";
+	
+}
+function function3(){
+	var x = document.getElementById("scroltxt");
+	x.style.fontFamily="Old Standard TT";
+	
+}
+function function4(){
+	var x = document.getElementById("scroltxt");
+	x.style.fontFamily="Comfortaa";
+	
+	
+}
